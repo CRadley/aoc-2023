c11bd25c31cf0ed60f0aa625bdd08b5bde7b0d9e
CRadley <61577912+CRadley@users.noreply.github.com>
2023-12-04 09:15:04 +0000

    Small day 4 refactor

--- d04.py
+++ d04.py
@@ -1,23 +1,22 @@
 import re
 import math
 
-with open("inputs/d04") as file:
+with open("input") as file:
     cards = [re.split("\s+", line) for line in file.read().split("\n")]
     pipe_index = cards[0].index("|")
 
 p1 = 0
-p2 = {i + 1: 1 for i in range(len(cards))}
+p2 = {i: 1 for i in range(len(cards))}
 
 for i, card in enumerate(cards):
-    card_id = i + 1
     numbers = list(map(int, filter(lambda x: all(y.isdigit() for y in x), card)))
     winning = set(numbers[: pipe_index - 2])
     personal = set(numbers[pipe_index - 2 :])
     matches = winning & personal
     if matches:
         p1 += int(math.pow(2, len(matches) - 1))
-    for i in range(len(matches)):
-        p2[i + 1 + card_id] += p2[card_id]
+    for j in range(len(matches)):
+        p2[i + j + 1] += p2[i]
 
 print(p1)
 print(sum(p2.values()))
