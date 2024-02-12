37454cdc9797e454848e66c5411adcb2845bd74d
Connor Radley <connor.radley@hotmail.co.uk>
2024-02-12 19:14:39 +0000

    refactor: Update day 5 to use functions

--- d05.py
+++ d05.py
@@ -1,59 +1,74 @@
+from typing import List
+
+
 with open("inputs/d05") as file:
     lines = [line for line in file.read().splitlines() if line]
 
-seeds = list(map(int, lines[0].split(" ")[1:]))
 
-maps = []
-current_map = []
-
-for line in lines[2:]:
-    if "map" in line:
-        maps.append(current_map[:])
-        current_map = []
-    else:
-        current_map.append(list(map(int, line.split(" "))))
-    if line == lines[-1]:
-        maps.append(current_map[:])
+def determine_seeds(seed_line: str) -> List[int]:
+    return list(map(int, seed_line.split(" ")[1:]))
 
 
-ranges = [[] for _ in range(7)]
-for i, m in enumerate(maps):
-    for dest_start, source_start, length in m:
-        ranges[i].append(
-            (
-                source_start,
-                source_start + length,
-                dest_start,
-                length,
-                dest_start + length,
+def determine_seed_ranges(seeds: List[int]) -> List[range]:
+    return [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
+
+
+def determine_map_ranges(map_lines: List[str]):
+    maps = []
+    current_map = []
+    for line in map_lines:
+        if "map" in line:
+            maps.append(current_map[:])
+            current_map = []
+        else:
+            current_map.append(list(map(int, line.split(" "))))
+        if line == lines[-1]:
+            maps.append(current_map[:])
+    ranges = [[] for _ in range(7)]
+    for i, m in enumerate(maps):
+        for dest_start, source_start, length in m:
+            ranges[i].append(
+                (
+                    source_start,
+                    source_start + length,
+                    dest_start,
+                    length,
+                    dest_start + length,
+                )
             )
-        )
-
-p1 = []
-for seed in seeds:
-    current = seed
-    for _r in ranges:
-        for r in _r:
-            if r[0] <= current <= r[1]:
-                current += r[2] - r[0]
-                break
-    p1.append(current)
+    return ranges
 
 
-seed_ranges = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
-p2 = 0
-found = False
-while not found:
-    current = p2
-    for _r in ranges[::-1]:
-        for r in _r:
-            if r[2] <= current < r[4]:
-                current -= r[2] - r[0]
-                break
-    for sr in seed_ranges:
-        if sr[0] <= current <= sr[1]:
-            found = True
-            break
-    if not found:
+def part_one(seeds: List[int], ranges) -> int:
+    p1 = []
+    for seed in seeds:
+        current = seed
+        for _r in ranges:
+            for r in _r:
+                if r[0] <= current <= r[1]:
+                    current += r[2] - r[0]
+                    break
+        p1.append(current)
+    return min(p1)
+
+
+def part_two(seed_ranges: List[range], ranges) -> int:
+    p2 = 0
+    while True:
+        current = p2
+        for _r in ranges[::-1]:
+            for r in _r:
+                if r[2] <= current < r[4]:
+                    current -= r[2] - r[0]
+                    break
+        for sr in seed_ranges:
+            if sr[0] <= current <= sr[1]:
+                return p2
         p2 += 1
-print(p2)
+
+
+seeds = determine_seeds(lines[0])
+seed_ranges = determine_seed_ranges(seeds)
+ranges = determine_map_ranges(lines[2:])
+assert part_one(seeds, ranges) == 35
+assert part_two(seed_ranges, ranges) == 46
