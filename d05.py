19fc60f088c74588778bdf9e328a68cc3736147e
CRadley <61577912+CRadley@users.noreply.github.com>
2024-02-12 21:48:05 +0000

    5 day 5 solution v2 (#6)
    * refactor: Update day 5 to use functions
    
    * refactor: Make ranges actual ranges
    
    * Add range based solution for d5, too slow for real data
    
    * Attempt to optimise with makeshift ranges
    
    * Add optimised solution
    
    * Add better names, remove silly typehints
    
    * Remove unused import

--- d05.py
+++ d05.py
@@ -1,59 +1,88 @@
 with open("inputs/d05") as file:
     lines = [line for line in file.read().splitlines() if line]
 
-seeds = list(map(int, lines[0].split(" ")[1:]))
 
-maps = []
-current_map = []
-
-for line in lines[2:]:
-    if "map" in line:
-        maps.append(current_map[:])
-        current_map = []
-    else:
-        current_map.append(list(map(int, line.split(" "))))
-    if line == lines[-1]:
-        maps.append(current_map[:])
+def determine_seeds(seed_line):
+    return list(map(int, seed_line.split(" ")[1:]))
 
 
-ranges = [[] for _ in range(7)]
-for i, m in enumerate(maps):
-    for dest_start, source_start, length in m:
-        ranges[i].append(
-            (
-                source_start,
-                source_start + length,
-                dest_start,
-                length,
-                dest_start + length,
+def determine_seed_ranges(seeds):
+    return [(seeds[i], seeds[i] + seeds[i + 1] - 1) for i in range(0, len(seeds), 2)]
+
+
+def determine_map_ranges(map_lines):
+    maps = []
+    current_map = []
+    for line in map_lines:
+        if "map" in line:
+            maps.append(current_map[:])
+            current_map = []
+        else:
+            current_map.append(list(map(int, line.split(" "))))
+        if line == lines[-1]:
+            maps.append(current_map[:])
+    ranges = [[] for _ in range(7)]
+    for i, m in enumerate(maps):
+        for dest_start, source_start, length in m:
+            ranges[i].append(
+                (
+                    source_start,
+                    source_start + length - 1,
+                    dest_start - source_start,
+                )
             )
-        )
-
-p1 = []
-for seed in seeds:
-    current = seed
-    for _r in ranges:
-        for r in _r:
-            if r[0] <= current <= r[1]:
-                current += r[2] - r[0]
-                break
-    p1.append(current)
+    return ranges
 
 
-seed_ranges = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
-p2 = 0
-found = False
-while not found:
-    current = p2
-    for _r in ranges[::-1]:
-        for r in _r:
-            if r[2] <= current < r[4]:
-                current -= r[2] - r[0]
-                break
-    for sr in seed_ranges:
-        if sr[0] <= current <= sr[1]:
-            found = True
-            break
-    if not found:
-        p2 += 1
-print(p2)
+def part_one(seeds, ranges):
+    p1 = []
+    for seed in seeds:
+        current = seed
+        for _r in ranges:
+            for start, end, offset in _r:
+                if start <= current <= end:
+                    current += offset
+                    break
+        p1.append(current)
+    return min(p1)
+
+
+def determine_crossovers(r1_min, r1_max, r2_min, r2_max):
+    if r1_min >= r2_min and r1_max <= r2_max:
+        return (r1_min, r1_max), None, None
+    if r1_min < r2_min and r2_min <= r1_max <= r2_max:
+        return (r2_min, r1_max), (r1_min, r2_min - 1), None
+    if r2_max > r1_min >= r2_min and r1_max > r2_max:
+        return (r1_min, r2_max), None, (r2_max + 1, r1_max)
+    if r1_min < r2_min and r2_max < r1_max:
+        return (r2_min, r2_max), (r1_min, r2_min - 1), (r2_max + 1, r1_max)
+    return None, None, None
+
+
+def part_two(seed_ranges, maps):
+    next_queue = seed_ranges[:]
+    for m in maps:
+        queue = next_queue[:]
+        next_queue = []
+        while queue:
+            current = queue.pop(0)
+            for r in m:
+                subrange, lower, upper = determine_crossovers(*current, r[0], r[1])
+                if not subrange and not lower and not upper and r == m[-1]:
+                    next_queue.append(current)
+                if lower:
+                    queue.append(lower)
+                if upper:
+                    queue.append(upper)
+                if subrange:
+                    next_queue.append((subrange[0] + r[2], subrange[1] + r[2]))
+                    break
+        queue = next_queue[:]
+    return min(_p[0] for _p in next_queue)
+
+
+seeds = determine_seeds(lines[0])
+seed_ranges = determine_seed_ranges(seeds)
+ranges = determine_map_ranges(lines[2:])
+print(part_one(seeds, ranges))
+print(part_two(seed_ranges, ranges))
