23e09e488e572d6d669b6aa9681e82896f8e1382
Connor Radley <connor.radley@hotmail.co.uk>
2024-02-12 20:56:08 +0000

    Attempt to optimise with makeshift ranges

--- d05.py
+++ d05.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from functools import cache
 
 with open("inputs/d05") as file:
     lines = [line for line in file.read().splitlines() if line]
@@ -10,7 +10,7 @@ def determine_seeds(seed_line: str) -> List[int]:
 
 
 def determine_seed_ranges(seeds: List[int]) -> List[range]:
-    return [range(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
+    return [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
 
 
 def determine_map_ranges(map_lines: List[str]):
@@ -29,8 +29,8 @@ def determine_map_ranges(map_lines: List[str]):
         for dest_start, source_start, length in m:
             ranges[i].append(
                 (
-                    range(source_start, source_start + length),
-                    range(dest_start, dest_start + length),
+                    source_start,
+                    source_start + length,
                     dest_start - source_start,
                 )
             )
@@ -42,43 +42,48 @@ def part_one(seeds: List[int], ranges) -> int:
     for seed in seeds:
         current = seed
         for _r in ranges:
-            for source, _, offset in _r:
-                if current in source:
+            for start, end, offset in _r:
+                if start <= current < end:
                     current += offset
                     break
         p1.append(current)
     return min(p1)
 
 
-def determine_crossover(prev_range: range, next_range: range) -> range:
-    return range(
-        max(prev_range[0], next_range[0]), min(prev_range[-1], next_range[-1]) + 1
-    )
+def determine_crossovers(r1_min, r1_max, r2_min, r2_max):
+    if r1_min >= r2_min and r1_max <= r2_max:
+        return (r1_min, r1_max), None, None
+    if r1_min < r2_min and r2_min <= r1_max <= r2_max:
+        return (r2_min, r1_max), (r1_min, r1_max - 1), None
+    if r2_max > r1_min >= r2_min and r1_max > r2_max:
+        return (r1_min, r2_max), None, (r2_max, r1_max)
+    if r1_min < r2_min and r2_max < r1_max:
+        return (r2_min, r2_max), (r1_min, r2_min - 1), (r2_max, r1_max)
+    return None, None, None
 
 
 def part_two(seed_ranges: List[range], maps) -> int:
     current_ranges = seed_ranges[:]
     for i, m in enumerate(maps):
-        print(i)
         next_ranges = []
         queue = current_ranges[:]
-        while queue:
+        while len(queue) > 0:
             current = queue.pop(0)
-            print(current)
             for r in m:
-                crossover = determine_crossover(current, r[0])
-                if len(crossover) == 0:
-                    if r == m[-1]:
-                        next_ranges.append(current)
-                    continue
-                elif crossover == current:
-                    next_ranges.append(range(current[0] + r[2], current[-1] + r[2] + 1))
+                subrange, lower_range, upper_range = determine_crossovers(
+                    *current, r[0], r[1]
+                )
+                if not subrange and not lower_range and not upper_range and r == m[-1]:
+                    next_ranges.append(current)
+                if subrange:
+                    new = (subrange[0] + r[2], subrange[1] + r[2])
+                    next_ranges.append(new)
+                if lower_range:
+                    queue.append(lower_range)
+                if upper_range:
+                    queue.append(upper_range)
+                if subrange or lower_range or upper_range:
                     break
-                else:
-                    if min(current) == min(crossover):
-                        queue.append(range(max(crossover) + 1, max(current) + 1))
-                    else:
-                        queue.append(range(min(current), min(crossover)))
         current_ranges = next_ranges[:]
     return min(min(r) for r in current_ranges)
 
@@ -88,10 +93,7 @@ seed_ranges = determine_seed_ranges(seeds)
 ranges = determine_map_ranges(lines[2:])
 print("P1 Start")
 p1 = part_one(seeds, ranges)
-print("P1 Finish")
+print(p1)
 print("P2 Start")
 p2 = part_two(seed_ranges, ranges)
-print("P2 Finish")
-
-assert p1 == 551761867, p1
-assert p2 == 57451709, p2
+print(p2)
